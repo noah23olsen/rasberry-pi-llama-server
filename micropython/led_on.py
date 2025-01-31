30e4d8b7d4a128bedba3f54cc57f291b2b2a8d10
Noah Olsen <noah23olsen@gmail.com>
2025-01-30 22:05:37 -0500

    working buzzer & led implementations

--- micropython/led_on.py
+++ micropython/led_on.py
@@ -1,3 +1,23 @@
-from picozero import pico_led
+from picozero import LED
+from time import sleep
+from machine import Pin
 
-pico_led.on()
+yellow = LED(14, Pin.OUT)
+
+while True:
+    yellow.on()
+    sleep(1)
+    yellow.off()
+    sleep(1)
+    
+
+#from machine import Pin
+#from time import sleep
+
+#buzzer = Pin(14, Pin.OUT)  # Connect to GPIO 14
+
+#while True:
+ #   buzzer.on()  # Turn the buzzer on
+  #  sleep(0.1)     # Wait for 1 second
+   # buzzer.off() # Turn the buzzer off
+    #sleep(1)   
